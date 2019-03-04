ebe5dde438e4c0a2df27ad7c8902d1e37c63da39
cur1576 <cur1576@gmx.de>
2019-03-04 10:17:11 +0100

    commit_04.03_01

--- app/src/main/java/com/example/mybroadcastreceiver/MyReceiver.java
+++ app/src/main/java/com/example/mybroadcastreceiver/MyReceiver.java
@@ -1,11 +1,14 @@
 package com.example.mybroadcastreceiver;
 
 import android.app.Notification;
+import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
+import android.support.v4.app.NotificationCompat;
 
 import java.text.DateFormat;
 import java.util.Date;
@@ -13,24 +16,41 @@ import java.util.Date;
 public class MyReceiver extends BroadcastReceiver {
 
     private static final int ID = 123;
+    private final String CHANNEL_ID = "chnnel_1";
+    CharSequence name = "MyChannel";
+    int importance = NotificationManager.IMPORTANCE_HIGH;
     @Override
     public void onReceive(Context context, Intent intent) {
         String msg = DateFormat.getDateTimeInstance().format(new Date());
+        NotificationManager notificationManager =
+                (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        NotificationChannel channel = null;
+        if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.O){
+            channel = new NotificationChannel(CHANNEL_ID,name,importance);
+            notificationManager.createNotificationChannel(channel);
+        }
+
         switch (intent.getAction()){
             case Intent.ACTION_AIRPLANE_MODE_CHANGED:
-                Notification.Builder builder = new Notification.Builder(context);
+                NotificationCompat.Builder builder = new NotificationCompat.Builder(context,CHANNEL_ID);
                 builder.setSmallIcon(R.mipmap.ic_launcher);
                 builder.setContentTitle(context.getString(R.string.app_name))
                         .setContentText(msg + " Flugmodus geändert!")
                         .setWhen(System.currentTimeMillis());
+
+                if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+
+                    builder.setChannelId(CHANNEL_ID);
+
+                }
 
                 Intent myIntent = new Intent(context, MainActivity.class);
                 PendingIntent pendingIntent = PendingIntent.getBroadcast(context,1,myIntent,
                         PendingIntent.FLAG_UPDATE_CURRENT);
                 builder.setContentIntent(pendingIntent);
                 Notification notification = builder.build();
-                NotificationManager notificationManager =
-                        (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+
                 notificationManager.notify(ID,notification);
         }
     }
